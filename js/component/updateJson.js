16d979ef06851f623c43f55a8ebb9ad7140078f8
MoahmedALNaser <mo7ammednaser092@gmail.com>
2023-07-12 12:27:51 +0300

    update html and add new page

---
+++ js/component/updateJson.js
@@ -0,0 +1,39 @@
+function getMeta(metaName) {
+  const metas = document.getElementsByTagName("meta");
+
+  for (let i = 0; i < metas.length; i++) {
+    if (metas[i].getAttribute("name") === metaName) {
+      return metas[i].getAttribute("content");
+    }
+  }
+
+  return "";
+}
+const loadFlage = true;
+const updateServices = async (e) => {
+  let padgeId = Number(getMeta("pageId"));
+  let view = Number(
+    document.querySelector(
+      ".wp-content .home .entery-header .info > span span.views"
+    ).textContent
+  );
+  console.log(view);
+  if (loadFlage) {
+    view = view + 1;
+    const doc = {
+      id: padgeId,
+      views: view,
+    };
+
+    fetch(`http://localhost:3000/servise/${padgeId}`, {
+      method: "PATCH",
+      body: JSON.stringify(doc),
+      headers: { "content-Type": "application/json" },
+    });
+    loadFlage = false;
+  }
+};
+// window.addEventListener("load", () => updateServices());
+// setTimeout(() => {
+//   updateServices();
+// }, 1000);
